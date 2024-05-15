ee5ca0b81f8bb532e203fb4d94975e06dfb5597b
DRAIA Oussama <100196763+Droronoaa@users.noreply.github.com>
2024-05-15 10:02:18 +0100

    creating front

--- ebanking-frontend/src/app/app.component.spec.ts
+++ ebanking-frontend/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'ebanking-frontend' title`, () => {
+  it(`should have the 'frontendDbanking' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ebanking-frontend');
+    expect(app.title).toEqual('frontendDbanking');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ebanking-frontend');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, frontendDbanking');
   });
 });
